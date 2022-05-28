c1f4c81c4c0604e5d83df966995d6a1b3c1f428f
sweetARTdev <93413701+sweetARTdev@users.noreply.github.com>
2022-05-29 00:01:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(hello, check):
+    # [assignment] Add your code here
+    if(sorted(hello)==sorted(check))
+    return True
+    else return False
+
